7c5e563a2f089a841997f4f146ae1dba1418b300
Nina Nguyen <nina.ntt2@gmail.com>
2018-12-02 17:08:11 +0100

    updated with CD

---
+++ CommandPattern/CommandPattern/QueueingService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandPattern
+{
+    class QueueingService
+    {
+        private readonly ArrayList _ordersQueue = new ArrayList();
+
+        public void PlaceOrder(IOrder order)
+        {
+            _ordersQueue.Add(order);
+            order.Execute();
+            _ordersQueue.Remove(_ordersQueue[0]);
+        }
+    }
+}
